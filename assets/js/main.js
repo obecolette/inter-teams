c9b7a151d84b03759651fee6fcde2c47da3f7a69
obecolette <rebecca.reese@gmail.com>
2015-11-03 23:56:26 -0500

    schick first commit

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+console.log("ready2go!")
+
+$(document).on("ready", function(e) {
+	// $(".card").flip('toggle');
+	// $(".card").flip({
+	// 	axis: 'x',
+	// 	speed: 400
+	// });
+$(".icon-dots").click(function() {
+    var thisCaption = $(".icon-dots").index($(this));
+
+    $(".caption").eq(thisCaption).toggle("slow",function() {});
+
+});
+
+
+	// $(".").hide();
+	console.log("hide");
+
+	// $(".icon-dots").on('click', function(){
+		// $(".card").flip('toggle');
+		// $(".card").flip({
+		// 	axis: 'x',
+		// 	speed: 400
+		// });
+
+
+		e.preventDefault();
+		var thisCaption = $(".icon-dots").index($(this));
+		console.log(thisCaption);
+
+		$(".caption").eq(thisCaption).toggle();
+		$(".caption").hide();
+});
