86c01daa39d8d4dbdc2a23ba23d7462af6b2ac76
obecolette <rebecca.reese@gmail.com>
2015-11-04 12:19:59 -0500

    add grooming-tips page

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+	$('.content').click(function(){
+		$(this).toggleClass('expand');
+
+	});
+
+	$('.mobile-navigation-menu').click(function(){
+		$('.mobile-menu').toggleClass('expand');
+
+	});
+
+});
+
